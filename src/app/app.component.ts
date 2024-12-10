822d3134dd47417f6d0f2fc4ab51dc48efe1eb9e
Rahul Kumar <rr03597@gmail.com>
2024-12-10 15:59:57 +0530

    Create new angular 19 food delivery  project

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet,MatCardModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'projectOne-Dec';
+}
